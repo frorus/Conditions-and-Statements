82ca86d6a006e451f88dc5a75d768a6310577da8
Ilya Silkin <zerolar@gmail.com>
2021-04-17 19:27:12 +0300

    Добавьте файлы проекта.

---
+++ Conditions-and-Statements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conditions_and_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
